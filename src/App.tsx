ad04f0bd40205a13aadef41c2bba7c75e70d9a98
Slawek <61154596+Ultran@users.noreply.github.com>
2021-11-20 19:08:12 +0100

    add three action creators and finish basick counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 // import { Counter } from "./features/counter/Counter";
 import "./App.css";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import {
+  incremented,
+  subtracted,
+  amountAdded,
+} from "./features/counter/counterSlice";
 
 function App() {
-  return <div className="App"></div>;
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  const handleClick = () => dispatch(incremented());
+  const handleSubtraction = () => dispatch(subtracted());
+  const handleAddAmount = () => dispatch(amountAdded(5));
+  return (
+    <div className="App">
+      <button onClick={handleClick}>increment</button>
+      <button onClick={handleSubtraction}>subtract</button>
+      <button onClick={handleAddAmount}>add5</button>
+
+      <h2>{count}</h2>
+    </div>
+  );
 }
 
 export default App;
--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface CounterState {
   value: number;
@@ -15,8 +15,14 @@ const counterSlice = createSlice({
     incremented(state) {
       state.value++;
     },
+    subtracted(state) {
+      state.value--;
+    },
+    amountAdded(state, action: PayloadAction<number>) {
+      state.value += action.payload;
+    },
   },
 });
 
-export const { incremented } = counterSlice.actions;
+export const { incremented, subtracted, amountAdded } = counterSlice.actions;
 export default counterSlice.reducer;
